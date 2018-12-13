92ead890a1ea750dd63fb2645e3c5dc849d8beba
Ngo <Danny.Ngo@city.ac.uk>
2018-12-13 13:31:45 +0000

    remove unused scripts

--- src/methods/sortJson.js
+++
@@ -1,9 +0,0 @@
-exports.sortJson = function(j) {
-    "use strict";
-    var s = [].concat(...r);
-    s.sort(function(a, b) {
-        return new Date(b.timeCreated * 1000) - new Date(a.timeCreated * 1000);
-    });
-
-    return s
-}
--- src/routes/date/date.js
+++
@@ -1,8 +0,0 @@
-const get = require('../../methods/getMethods');
-
-module.exports = (req, res) => {
-  const modelId = req.query.itemRef;
-  const model = get.getJsonById(modelId)
-
-  res.status(200).json( model );
-};
